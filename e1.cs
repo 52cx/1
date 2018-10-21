e39225ba66f961b0ea8c4be49012d273284f5c67
czklovecx <43518235+czk042808@users.noreply.github.com>
2018-10-21 20:13:59 +0800

    Create e1.cs

---
+++ e1.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace e1
+
+{
+
+    /// <summary>
+
+    /// Class1 的摘要说明。
+
+    /// </summary>
+
+    class Class1
+
+    {
+
+        /// <summary>
+
+        /// 应用程序的主入口点。
+
+        /// </summary>
+
+        [STAThread]
+
+        static void Main(string[] args)
+
+        {
+
+            //
+
+            // TODO: 在此处添加代码以启动应用程序
+
+            //
+
+            Console.WriteLine("请键入你的姓名：");
+
+            Console.ReadLine();
+
+            Console.WriteLine("欢迎！");
+
+        }
+
+    }
+
+}
+
